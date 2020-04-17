6cbdca1b92f88334e3260a9f1cd2a5b2b1492d5f
icebeam7 <luis.beltran@itcelaya.edu.mx>
2020-04-17 21:36:26 +0200

    Initial version

---
+++ LocalFaceApiDemo/Helpers/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LocalFaceApiDemo.Helpers
+{
+    public static class Constants
+    {
+        public static readonly string LocalEndpoint = "http://192.168.1.6:5000/";
+        public static readonly string LocalService = "face/v1.0/detect";
+        public static readonly string RequestParameters = "returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=gender%2Cage%2Csmile%2Cglasses%2Cemotion&returnRecognitionModel=false";
+    }
+}
---
+++ LocalFaceApiDemo/Services/FaceService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+using LocalFaceApiDemo.Models;
+using LocalFaceApiDemo.Helpers;
+
+using Newtonsoft.Json;
+using Plugin.Media.Abstractions;
+
+namespace LocalFaceApiDemo.Services
+{
+    public static class FaceService
+    {
+        private static readonly HttpClient client = CreateHttpClient();
+
+        private static HttpClient CreateHttpClient()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(Constants.LocalEndpoint);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
+
+        public async static Task<FaceInfo> AnalyzePhoto(MediaFile photo)
+        {
+            try
+            {
+                var content = new StreamContent(photo.GetStream());
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+
+                var response = await client.PostAsync($"{Constants.LocalService}?{Constants.RequestParameters}", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<FaceInfo[]>(json);
+                    return result.FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return new FaceInfo();
+        }
+    }
+}
